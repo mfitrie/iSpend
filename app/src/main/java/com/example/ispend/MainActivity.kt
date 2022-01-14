bb2980282629e593b2bb9a4df508aabb6020905c
mfitrie <mfitrie78@gmail.com>
2022-01-14 21:36:08 +0800

    make function to adapter in main

--- app/src/main/java/com/example/ispend/MainActivity.kt
+++ app/src/main/java/com/example/ispend/MainActivity.kt
@@ -1,34 +1,47 @@
 package com.example.ispend
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var spendList = mutableListOf(
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-            Spend(20.00, "Food", "20/2/2021"),
-        )
+        insertingToSpendAdapter(rvSpend, this)
 
-        val adapter = SpendAdapter(spendList)
-        rvSpend.adapter = adapter
-        rvSpend.layoutManager = LinearLayoutManager(this)
-
-
-        adapter.notifyItemInserted(spendList.size -1)
 
     }
+
+
+}
+
+fun insertingToSpendAdapter(rv: RecyclerView, context: Context){
+    var spendList = mutableListOf(
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+        Spend(20.00, "Food", "20/2/2021"),
+    )
+
+    val adapter = SpendAdapter(spendList)
+
+    rv.adapter = adapter
+    rv.layoutManager = LinearLayoutManager(context)
+//    rvSpend.adapter = adapter
+//    rvSpend.layoutManager = LinearLayoutManager(this)
+
+
+    adapter.notifyItemInserted(spendList.size -1)
 }
